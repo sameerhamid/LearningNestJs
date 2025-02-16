2a0419d33749abbf3abae28430b436882e0739b6
apple <apple@apples-MacBook-Air.local>
2025-02-16 13:27:13 +0530

    add oneToOne relationShip with typeorm

--- src/property/entities/property.entity.ts
+++ src/property/entities/property.entity.ts
@@ -1,5 +1,6 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { number } from 'zod';
+import { PropertyFeature } from './propertyFeature.entity';
 
 @Entity()
 export class Property {
@@ -22,4 +23,11 @@ export class Property {
     default: 0,
   })
   price: number;
+
+  @OneToOne(
+    () => PropertyFeature,
+    (propertyFeature) => propertyFeature.property,
+    { cascade: true },
+  )
+  propertyFeature: PropertyFeature;
 }

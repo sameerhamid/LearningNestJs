1c99052b73d0fc7cdd8375306030b281b7904cd0
apple <apple@apples-MacBook-Air.local>
2025-02-22 12:00:37 +0530

    add role data in request user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -6,6 +6,7 @@ import { AuthJwtPayload } from './types/auth-jwtPayload';
 import refreshJwtConfig from './config/refresh-jwt.config';
 import { ConfigType } from '@nestjs/config';
 import * as argon2 from 'argon2';
+import { CurrentUser } from './types/current-user';
 @Injectable()
 export class AuthService {
   constructor(
@@ -88,5 +89,17 @@ export class AuthService {
 
   async signOut(userId: number) {
     await this.userService.updateHashedRefreshToken(userId, '');
+  }
+
+  async validateJwtUser(userId: number) {
+    const user = await this.userService.findOne(userId);
+    if (!user) {
+      throw new UnauthorizedException('User not found');
+    }
+    const currentUser: CurrentUser = {
+      id: user.id,
+      role: user.role,
+    };
+    return currentUser;
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -34,6 +34,7 @@ export class UserService {
         'email',
         'avatarUrl',
         'hashedRefreshToken',
+        'role',
       ],
     });
   }

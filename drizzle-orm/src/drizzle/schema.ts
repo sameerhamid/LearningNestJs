e89f7cf3fac7ae6d386ac55fffee6f7321a89a1d
apple <apple@apples-MacBook-Air.local>
2025-02-23 11:17:16 +0530

    add implecit one to one realtion ship

--- drizzle-orm/src/drizzle/schema.ts
+++ drizzle-orm/src/drizzle/schema.ts
@@ -1,8 +1,14 @@
-import { pgTable, serial, text } from 'drizzle-orm/pg-core';
+import { integer, jsonb, pgTable, serial, text } from 'drizzle-orm/pg-core';
 
 export const Users = pgTable('users', {
   id: serial('id').primaryKey(),
   name: text('name').notNull(),
   email: text('email').notNull(),
   password: text('password').notNull(),
+});
+
+export const profileInfo = pgTable('profileInfo', {
+  id: serial('id').primaryKey(),
+  metadata: jsonb('metadata'),
+  userId: integer('userId').references(() => Users.id),
 });

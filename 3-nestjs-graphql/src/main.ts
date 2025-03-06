38b54ef1ebadc909eccaf3cfeee479ca6d270e40
apple <apple@apples-MacBook-Air.local>
2025-03-06 22:09:31 +0530

    input validation in graphql

--- 3-nestjs-graphql/src/main.ts
+++ 3-nestjs-graphql/src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { EntityNotFoundFilter } from './entity-not-found/entity-not-found.filter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Register the global exception filter
   app.useGlobalFilters(new EntityNotFoundFilter());
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- 3-nestjs-graphql/src/user/dto/create-user-input.ts
+++ 3-nestjs-graphql/src/user/dto/create-user-input.ts
@@ -1,10 +1,14 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { IsEmail, IsString } from 'class-validator';
 
 @InputType()
 export class CreateUserInput {
+  @IsString()
   @Field(() => String)
   username: string;
 
+  @IsString()
+  @IsEmail()
   @Field(() => String)
   email: string;
 }
